2a75fff232b5ec8eb85ac0aa05c367b60102c922
julie_moon <gossingg@naver.com>
2018-11-14 13:18:55 +0900

    basic_event_임의 메소드

--- basic/src/EventPractice.js
+++ basic/src/EventPractice.js
@@ -6,40 +6,47 @@ class EventPractice extends Component{
         message:''
     }
 
+    /*  
+        임의 메소드 만들기
+
+        1) 기본 방법
+        컴포넌트에 임의 메소드를 만들면 기본적으로 this에 접근할 수 없음
+        따라서 컴포넌트 생성자 constructor에서 각 메소드를 this와 바인딩 해줘야 함.
+        즉, 메소드에서 this를 사용할 수 있도록 메소드에 this를 묶어줌.
+
+        constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        }
+
+        2) 화살표 함수 형태로 메소드 정의
+    */
+    
+    handleChange = (e) => {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleClick = () => {
+        alert(this.state.message);
+        this.setState({
+            message:''
+        })
+    }
+
     render(){
         return(
             <div>
                 <h1>이벤트 연습</h1>
-                {/* 
-                    이벤트 사용 시 주의할 점
-                    1) 이벤트명은 camelCase로 작성 ex> onClick, onKeyUp..
-                    2) 함수 형태의 값을 전달
-                    3) DOM 요소에만 이벤트 설정 가능
-                */}
                 <input type='text'
                        name='message'
                        placeholder='아무거나 입력해보세요'
                        value={this.state.message}
-                       /* 
-                            e 객체 : SyntheticEvent
-                                     웹 브라우저의 네이티브 이벤트를 감싸는 객체 
-                       */
-                       onChange={(e) => 
-                            {
-                                this.setState({
-                                    message:e.target.value
-                                })
-                            }
-                       }
+                       onChange={this.handleChange}
                 />
-                <button onClick={() => 
-                    {
-                        alert(this.state.message);
-                        this.setState({
-                            message:''
-                        });
-                    }
-                }>확인</button>
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
