f4969a3a0b501b4ea301efabfe3ea63f595ac588
julie_moon <gossingg@naver.com>
2018-11-14 13:51:15 +0900

    basic_event_input 여러 개 핸들링

--- basic/src/EventPractice.js
+++ basic/src/EventPractice.js
@@ -2,49 +2,53 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component{
 
+    /* 
+        input 여러 개 핸들링
+        : event 객체를 활용
+    */
+
     state = {
+        username:'',
         message:''
     }
-
-    /*  
-        임의 메소드 만들기
-
-        1) 기본 방법
-        컴포넌트에 임의 메소드를 만들면 기본적으로 this에 접근할 수 없음
-        따라서 컴포넌트 생성자 constructor에서 각 메소드를 this와 바인딩 해줘야 함.
-        즉, 메소드에서 this를 사용할 수 있도록 메소드에 this를 묶어줌.
-
-        constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-        }
-
-        2) 화살표 함수 형태로 메소드 정의
-    */
     
     handleChange = (e) => {
         this.setState({
-            message: e.target.value
+            // [] 안에 있는 값을 key 값으로 사용함
+            [e.target.name]: e.target.value
         });
     }
 
     handleClick = () => {
-        alert(this.state.message);
+        alert(this.state.username + ' : ' + this.state.message);
         this.setState({
+            username:'',
             message:''
         })
+    }
+
+    handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            this.handleClick();
+        }
     }
 
     render(){
         return(
             <div>
                 <h1>이벤트 연습</h1>
+                <input type='text' 
+                       name='username'
+                       placeholder='유저명'
+                       value={this.state.username}
+                       onChange={this.handleChange}
+                />
                 <input type='text'
                        name='message'
                        placeholder='아무거나 입력해보세요'
                        value={this.state.message}
                        onChange={this.handleChange}
+                       onKeyPress={this.handleKeyPress}
                 />
                 <button onClick={this.handleClick}>확인</button>
             </div>
