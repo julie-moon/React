7b956866a536e621ed4e75bef526991ab224559e
julie_moon <gossingg@naver.com>
2018-11-21 12:49:39 +0900

    styling_styled-components

--- styling/src/App.js
+++ styling/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
     return (
-      <CSSModule />
+      <StyledComponent />
     );
   }
 }
