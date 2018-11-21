0ed142ad867d968326ce3ab4ec05254dac9a5584
julie_moon <gossingg@naver.com>
2018-11-21 10:50:28 +0900

    styling_CSSModule_클래스명 두 개 이상 적용하기

--- styling/src/CSSModule.js
+++ styling/src/CSSModule.js
@@ -3,7 +3,7 @@ import styles from './CSSModule.module.css';
 const CSSModule = () => {
     console.log(styles);
     return (
-        <div className={styles.wrapper}>
+        <div className={`${styles.wrapper} ${styles.inverted}`}>
             안녕하세요. 저는 <span className="something">CSS Module-!</span>
         </div>
     )
