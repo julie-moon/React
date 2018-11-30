dedc07dbbcc70cc1f94f2fb4613b84f73a02eb3e
julie_moon <gossingg@naver.com>
2018-11-30 19:25:57 +0900

    redux-counter_리덕스로 리액트 어플리케이션 state 관리_서브 리듀서 생성

--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -1,42 +1,27 @@
-import * as types from '../actions/ActionTypes';
+import number from './number';
+import color from './color';
+
+import { combineReducers } from 'redux';
 
 /* 
-    리듀서 : action type에 따라 변화를 일으키는 함수. 
-             리듀서를 작성할 때는 최초 변화를 일으키기 전
-             가지고 있어야 할 초기 상태를 정의해야 함
-*/
+    서브 리듀서를 하나로 합침.
+    combineReducers를 실행하고 나면, 나중에 store 형태를
+    파라미터로 전달한 객체 모형대로 만듦.
 
-// 초기 상태 정의
-const initialState = {
-    color:'#000',
-    number:0
-}
-
-/*
-    리듀서 함수 정의 
-    - 리듀서는 state와 action을 파라미터로 받음
-    - 스토어가 생성될 당시 state가 undefined일 때,
-      state의 기본값을 initialState로 사용
-    - action.type에 따라 다른 작업을 하고, 새 state를 만들어서 반환함
-      이때 주의할 점은 state를 직접 수정하면 안 되고,
-      기존 state 값을 덮어쓴 새로운 객체를 만들어서 반환해야 함
-
-*/
-function counter(state = initialState, action) {
-    switch (action.type) {
-        case types.INCREMENT: 
-            return {
-                ...state,
-                number:state.number + 1
-            };
-        case types.DECREMENT:
-            return {
-                ...state,
-                number:state.number - 1
-            };
-        default:
-            return state;
+    지금은 다음과 같이 만듦.
+    {
+        numberData:{
+            number:0
+        },
+        colorData:{
+            color:'#000'
+        }
     }
-}
+*/
 
-export default counter;
+const reducers = combineReducers({
+    numberData: number,
+    colorData : color
+})
+
+export default reducers;
