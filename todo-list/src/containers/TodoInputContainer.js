9954e9fded4aff730bf7974dca188c3f2253802b
julie_moon <gossingg@naver.com>
2018-12-05 19:17:17 +0900

    todo-list_토이 프로젝트_리덕스 적용

---
+++ todo-list/src/containers/TodoInputContainer.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import TodoInput from '../components/TodoInput';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// 액션 생성 함수들을 한꺼번에 불러옴
+import * as inputActions from '../modules/input';
+import * as todosActions from '../modules/todos';
+
+class TodoInputContainer extends Component{
+    id = 1
+    getId = () => {
+        return ++this.id;
+    }
+
+    handleChange = (e) => {
+        const { value } = e.target;
+        const { InputActions } = this.props;
+        InputActions.setInput(value);
+    }
+
+    handleInsert = () => {
+        const{ InputActions, TodosActions, value } = this.props;
+        const todo = {
+            id:this.getId(),
+            text:value,
+            done:false
+        }
+        TodosActions.insert(todo);
+        InputActions.setInput('');
+    }
+
+    render() {
+        const { value } = this.props;
+        const { handleChange, handleInsert } = this;
+        return (
+            <TodoInput onChange={handleChange}
+                       onInsert={handleInsert}
+                       value={value} />
+        )
+    }
+}
+
+/*
+    mapStateToProps와 mapDispatchToProps 함수에 대한
+    레퍼런스를 따로 만들지 않고, 그 내부에 바로 정의함
+*/
+export default connect( 
+    (state) => ({
+        value:state.input.get('value')
+    }),
+    (dispatch) => ({
+        /* 
+            bindActionCreators를 사용하면 자동으로 다음 작업들을 함
+            {
+                actionCreator:(...params) => dispatch(actionCreator(...params))
+            }
+            그래서 우리가 이전에 한 것처럼 일일이 dispatch를 할 필요가 없음.
+
+            예를 들어 InputActions에는 다음 작업들이 되어 있음
+            InputActions:{
+                setInput:(value) => dispatch(inputActions.setInput(value))
+            }
+
+            나중에 이를 호출할 때는 
+            this.props.InputActions.setInput을 호출하면 됨
+        */
+        InputActions:bindActionCreators(inputActions, dispatch),
+        TodosActions:bindActionCreators(todosActions, dispatch)
+    })
+)(TodoInputContainer)
